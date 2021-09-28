c95bb4dafa9eb02d17e74d9712c031eccfab6e3c
tinechitando <tinetchitsa@gmail.com>
2021-09-28 22:35:55 +0200

    created as js file

---
+++ about.js
@@ -0,0 +1,12 @@
+function subscription(){
+    alert("Thank you for joining our newsletter");
+  }
+    function bigger(sizeImg){
+        sizeImg.style.height = "300px";
+        sizeImg.style.width = "300px";
+      }
+      function normal(sizeImg){
+          sizeImg.style.height = "260px";
+          sizeImg.style.width = "260px";
+      }
+  
