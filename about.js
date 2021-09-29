87de0c0e94b65f71317731ee32a43ab5a356e97e
Tinomudaishe33 <christinemarumahoko3@gmail.com>
2021-09-29 13:11:08 +0200

    minor add

--- about.js
+++ about.js
@@ -1,6 +1,14 @@
 function subscription(){
-    alert("Thank you for joining our newsletter");
+  var emailInfo =document.getElementById("email");
+  if(emailInfo.value.trim() == "")
+  {
+      alert("please enter a valid email address");
+      
   }
+  else{
+  alert("Thank you for joining our newsletter");
+  }
+}
     function bigger(sizeImg){
         sizeImg.style.height = "300px";
         sizeImg.style.width = "300px";
